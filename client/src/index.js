b801e5a7efc0a1bce8746c78c0e7613130ac4a5f
AlmafuerteAriel <almafuerte.ariel.1978@gmail.com>
2021-12-22 14:49:00 -0300

    boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+// URL relativa -> local o deploy
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 ReactDOM.render(
   <React.StrictMode>

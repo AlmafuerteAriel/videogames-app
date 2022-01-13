3b698a11e1df986a99f5468889976d30e2dda59c
AlmafuerteAriel <almafuerte.ariel.1978@gmail.com>
2022-01-13 11:08:30 -0300

    landing-page fix

--- api/src/controllers/videogames.js
+++ api/src/controllers/videogames.js
@@ -9,7 +9,7 @@ const API_KEY = process.env;
 const videogames = async (req, res) => {
     try{
         const name = req.query.name;
-        const allVideogames = await axios.get(API_KEY);
+        const allVideogames = await axios.get(`https://api.rawg.io/api/games?key=c6f6a20e5c7847bd88e95f0e3fcc07d5`);
         res.json(allVideogames.data);
     } catch (error) {
         res.status(400).json({ msg: 'Server error'});

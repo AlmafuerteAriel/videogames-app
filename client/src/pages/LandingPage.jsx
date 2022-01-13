581265a9495d611b559144f87f1469e884a452a8
AlmafuerteAriel <almafuerte.ariel.1978@gmail.com>
2022-01-13 11:20:27 -0300

    landing-page fix2

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -8,11 +8,11 @@ import linkedin from '../assets/linkedin.png';
 export default function LandingPage() {
     return (
       <div className='landingContent'>
-        <img className='landingIcon' className='landingIcon1' src={molecule} alt="line" />
-        <img className='landingIcon' className='landingIcon2' src={molecule} alt="line" />
+        <img className='landingIcon1' src={molecule} alt="line" />
+        <img className='landingIcon2' src={molecule} alt="line" />
         <h1 className="landingTitle">VIDEOGAMES</h1>
-        <img className='landingIcon' className='landingIcon3' src={molecule} alt="line" />
-        <img className='landingIcon' className='landingIcon4' src={molecule} alt="line" />
+        <img className='landingIcon3' src={molecule} alt="line" />
+        <img className='landingIcon4' src={molecule} alt="line" />
         <Link to='/home'>
           <button className='landingButton'>
             <span>Press Start</span>
@@ -22,7 +22,7 @@ export default function LandingPage() {
           <a href="https://github.com/AlmafuerteAriel/" target="blank"><img className='landingPresIcon' src={github} alt="github" /></a>
           <a href="https://www.linkedin.com/in/ariel-alejandro-romero-14a43020b/" target="blank"><img className='landingPresIcon'src={linkedin} alt="linkedin" /></a>
         </div>
-        <img className='landingIcon' className='landingIcon5' src={molecule} alt="line" />
+        <img className='landingIcon5' src={molecule} alt="line" />
       </div>
     )
   }
